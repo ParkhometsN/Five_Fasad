ed6ecd1a27a59679d8595605b176ccd13eb8b959
Paarxomets <parxomez1@mail.ru>
2025-11-30 15:51:28 +0300

    add slider update 0.13

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ import { GetHeader } from "./modules/header.js";
 import { MessageChat } from "./modules/MessageChat.js";
 import { GetFooter } from "./modules/footer.js";
 import { hidePreloader } from "./modules/preloader.js";
-import { MainSlider } from "./modules/slider.js";
+import {initProjectSlider} from "./modules/slider.js"
 import { Form } from "./modules/comonents/Post_Form.js";
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
         GetHeader();
         MessageChat();
         GetFooter();
-        // MainSlider();
+        initProjectSlider()
         // Form();
     } catch (error) {
         console.error('Error initializing app:', error);
